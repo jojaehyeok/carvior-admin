e7c76442eea0a01a5aab737096a7eb079488e5f9
JoJaehyeok <jjhstc2285@naver.com>
2026-03-19 02:25:05 +0900

    dsadsa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   basePath: '/admin',
-  assetPrefix: '/admin', // 정적 파일 경로까지 확실히 지정
+  assetPrefix: 'https://carvior.store/admin', // 정적 파일 경로까지 확실히 지정
   reactStrictMode: true,
   transpilePackages: ["antd"],
+
 };
 
 module.exports = nextConfig;
+
