2ede97d032e9ff021835b11f7bbd771d9a3c06e8
JoJaehyeok <jjhstc2285@naver.com>
2026-03-19 02:14:29 +0900

    dsadsa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/admin',
+  assetPrefix: '/admin', // 정적 파일 경로까지 확실히 지정
   reactStrictMode: true,
   transpilePackages: ["antd"],
 };
